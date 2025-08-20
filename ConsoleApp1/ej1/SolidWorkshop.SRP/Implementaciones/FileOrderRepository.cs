87b58556484fe7f4bfbb632a8e1b0f59b67c4452
LoloColombo <187066052+LoloColombo@users.noreply.github.com>
2025-08-19 21:08:31 -0300

    Add files via upload

---
+++ ConsoleApp1/ej1/SolidWorkshop.SRP/Implementaciones/FileOrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolidWorkshop.SRP.interfaces;
+
+namespace SolidWorkshop.SRP.Implementaciones
+{
+    public class FileOrderRepository : IOrderRepository 
+    {
+        private readonly string _dbPath = "orders_db.txt";
+
+        public void Save(Order order)
+        {
+            // 3) Persistencia en un "archivo DB"
+            var line = $"{order.Id}|{order.CustomerEmail}|{order.Total}|{order.CreatedAt:o}";
+            File.AppendAllText(_dbPath, line + Environment.NewLine);
+        }
+    }
+}
---
+++ ConsoleApp1/ej1/SolidWorkshop.SRP/interfaces/IOrderValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolidWorkshop.SRP;
+
+namespace SolidWorkshop.SRP.interfaces
+{
+    public interface IOrderValidator
+    {
+        void Validation(Order order);
+    }
+}
